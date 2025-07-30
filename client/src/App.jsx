f4f8500d6bc89d62ab2513b27a0f8a4ef590879a
jimit.rawal <jimitrawal15@gmail.com>
2025-07-30 19:53:30 -0400

    Fixed main.py

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
 
 export default function App() {
-
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      {userName}
-    </h1>
+    <>
+    Testing
+    </>
   )
 }
