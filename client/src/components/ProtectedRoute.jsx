856018f3a8f41b8c73fe4ee462fdde0c3133cd2a
Om-1004 <opatel101004@gmail.com>
2025-10-03 00:09:48 -0400

    Protected Route FIxed

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const ProtectedRoute = ({ children }) => {
-    const isLoggedIn = localStorage.getItem("authToken")
-    if (!isLoggedIn) {
-        return <Navigate to="/login" replace />
+    const user = useSelector((state) => state.user);
+
+    console.log(user?.userName)
+    if (!user?.userName) {
+        return <Navigate to="/login" replace/>
     }
     return children;
 
